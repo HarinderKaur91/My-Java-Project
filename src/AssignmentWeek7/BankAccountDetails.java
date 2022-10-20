0b614c6a3229ef43db648df20c5d6afc866c97d9
HarinderKaur91 <harinder.kaur66@yahoo.com>
2022-10-20 15:45:04 -0600

    Week7 Assignment

---
+++ src/AssignmentWeek7/BankAccountDetails.java
@@ -0,0 +1,41 @@
+package AssignmentWeek7;
+
+public class BankAccountDetails {
+
+	int accNumber;
+	int accPin;
+	double accBalance;
+
+	public BankAccountDetails(int accountNumber, int accountPin, double accountBalance) {
+		accNumber = accountNumber;
+		accPin = accountPin;
+		accBalance = accountBalance;
+	}
+
+	boolean isUserCredentialsCorrect = false;
+
+	boolean checkAccountCredentials(int accountNumber, int accountPin) {
+		if (accountNumber == accNumber && accountPin == accPin) {
+			isUserCredentialsCorrect = true;
+		}
+		return isUserCredentialsCorrect;
+	}
+
+	void checkBalance() {
+		System.out.println("\nAccount Balance = " + accBalance);
+	}
+
+	void depositMoney(double depositAmount) {
+		if (depositAmount > 0.0) {
+			accBalance = accBalance + depositAmount;
+			System.out.println("Balance after the deposit of $" + depositAmount + " = $" + accBalance);
+		} else {
+			System.out.println("You cannot deposit negative amount. Start again.");
+		}
+	}
+
+	void withdrawMoney(double withdrawAmount) {
+		accBalance = accBalance - withdrawAmount;
+		System.out.println("Balance after the withdraw of $" + withdrawAmount + " = $" + accBalance);
+	}
+}
---
+++ src/AssignmentWeek7/MainClassBankAccount.java
@@ -0,0 +1,129 @@
+package AssignmentWeek7;
+
+import java.util.Scanner;
+
+public class MainClassBankAccount {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		BankAccountDetails user1 = new BankAccountDetails(1111, 1616, 100.00);
+		BankAccountDetails user2 = new BankAccountDetails(2222, 2626, 200.00);
+		BankAccountDetails user3 = new BankAccountDetails(3333, 3636, 300.00);
+		BankAccountDetails user4 = new BankAccountDetails(4444, 4646, 400.00);
+		BankAccountDetails user5 = new BankAccountDetails(5555, 5656, 500.00);
+
+		BankAccountDetails[] bankAccountDetails = { user1, user2, user3, user4, user5 };
+
+		int i;
+		int accountNumber = 0;
+		int accountPin = 0;
+		boolean isUserCredentialsCorrect = false;
+		int choiceOfDepositOrWithdraw = 0;
+		double depositAmount = 0.0;
+		double withdrawAmount = 0.0;
+		int numberOfAttempts = 0;
+		int numberOfWithdrawAttempts = 0;
+		boolean isBalanceInSufficient = false;
+
+		System.out.println("Enter the account number");
+		accountNumber = sc.nextInt();
+		System.out.println("Enter the pin");
+		accountPin = sc.nextInt();
+
+		for (i = 0; i < bankAccountDetails.length; i++) {
+			isUserCredentialsCorrect = bankAccountDetails[i].checkAccountCredentials(accountNumber, accountPin);
+			if (isUserCredentialsCorrect) {
+				bankAccountDetails[i].checkBalance();
+				break;
+			} else {
+				isUserCredentialsCorrect = false;
+			}
+		}
+		if (!isUserCredentialsCorrect) {
+			while (!isUserCredentialsCorrect) {
+				numberOfAttempts++;
+				if (numberOfAttempts > 2) {
+					System.out.println("Attempts exceeded! Start again");
+					System.exit(0);
+				}
+				System.out.println("INVALID username or pin.Enter valid credentials");
+				System.out.println("\nEnter the account number");
+				accountNumber = sc.nextInt();
+				System.out.println("Enter the pin");
+				accountPin = sc.nextInt();
+				for (i = 0; i < bankAccountDetails.length; i++) {
+					isUserCredentialsCorrect = bankAccountDetails[i].checkAccountCredentials(accountNumber, accountPin);
+					if (isUserCredentialsCorrect) {
+						bankAccountDetails[i].checkBalance();
+						break;
+					} else {
+						isUserCredentialsCorrect = false;
+					}
+				}
+			}
+		}
+
+		System.out.println("\nPress1 : Deposit Money");
+		System.out.println("Press2 : Withdraw Money");
+		choiceOfDepositOrWithdraw = sc.nextInt();
+
+		while (!(choiceOfDepositOrWithdraw == 1 || choiceOfDepositOrWithdraw == 2)) {
+			numberOfAttempts++;
+			if (numberOfAttempts > 2) {
+				System.out.println("Attempts exceeded! Start again");
+				System.exit(0);
+			}
+			System.out.println("INVALID INPUT! Enter 1 or 2");
+			choiceOfDepositOrWithdraw = sc.nextInt();
+		}
+
+		if (choiceOfDepositOrWithdraw == 1) {
+			System.out.println("Enter amount to be deposited");
+			depositAmount = sc.nextDouble();
+			for (i = 0; i < bankAccountDetails.length; i++) {
+				if ((accountNumber == bankAccountDetails[i].accNumber) && (accountPin == bankAccountDetails[i].accPin)) {
+					bankAccountDetails[i].depositMoney(depositAmount);
+					break;
+				}
+			}
+		} else if (choiceOfDepositOrWithdraw == 2) {
+			System.out.println("Enter amount to be withdrawn");
+			withdrawAmount = sc.nextDouble();
+			for (i = 0; i < bankAccountDetails.length; i++) {
+				if ((accountNumber == bankAccountDetails[i].accNumber) && (accountPin == bankAccountDetails[i].accPin)) {
+					if (bankAccountDetails[i].accBalance >= withdrawAmount) {
+						bankAccountDetails[i].withdrawMoney(withdrawAmount);
+						break;
+					} else {
+						isBalanceInSufficient = true;
+					}
+				}
+			}
+			while (isBalanceInSufficient) {
+				numberOfWithdrawAttempts++;
+				if (numberOfWithdrawAttempts > 2) {
+					System.out.println("Attempts exceeded! Start again");
+					System.exit(0);
+				}
+				System.out.println("You cannot withdraw money. Your balance is lesser than withdraw amount.");
+				System.out.println("Enter amount lesser or equal to the balance");
+				withdrawAmount = sc.nextDouble();
+				for (i = 0; i < bankAccountDetails.length; i++) {
+					if ((accountNumber == bankAccountDetails[i].accNumber)
+							&& (accountPin == bankAccountDetails[i].accPin)) {
+						if (bankAccountDetails[i].accBalance >= withdrawAmount) {
+							bankAccountDetails[i].withdrawMoney(withdrawAmount);
+							isBalanceInSufficient = false;
+							break;
+						} else {
+							isBalanceInSufficient = true;
+						}
+					}
+				}
+			}
+		}
+
+	}
+}
